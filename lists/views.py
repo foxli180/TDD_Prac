b974251a64b9f85d1a59aac903d41d2b801c4772
fox@home <jiaqingli@gmail.com>
2014-04-21 09:03:20 -0700

    before refactoring

--- lists/views.py
+++ lists/views.py
@@ -5,4 +5,9 @@ from django.http import HttpResponse
 #home_page = None
 
 def home_page(request):
-    return render(request, 'home.html')
+    '''
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
+    '''
+    return render(request, 'home.html',{
+                'new_item_text': request.POST.get('item_text',''),})
