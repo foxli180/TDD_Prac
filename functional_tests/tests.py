0b87f746b46116bdf2a0763d7c39c3dbab7a2cd8
fox1404_of <jiaqingli@gmail.com>
2014-04-23 16:49:10 +0800

    modify home.html to make the FT pass. missing action property in POST of home.html

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -39,13 +39,14 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
-        print(edith_list_url)
+        
+        
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
-        #inputbox =self.browser.find_element_by_id('id_new_item')
-        #inputbox.send_keys('Use peacock feathers to make a fly')
-        #inputbox.send_keys(Keys.ENTER) 
+        inputbox =self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER) 
         
         
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
