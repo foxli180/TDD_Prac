5d93ca21f917a0d26afc218773b2179283f7de5f
fox1404_of <jiaqingli@gmail.com>
2014-04-23 16:14:28 +0800

    new URL+view for adding to existsing lists, but FT fail

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -43,9 +43,9 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
-        inputbox =self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER) 
+        #inputbox =self.browser.find_element_by_id('id_new_item')
+        #inputbox.send_keys('Use peacock feathers to make a fly')
+        #inputbox.send_keys(Keys.ENTER) 
         
         
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
