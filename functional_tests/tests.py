217c8fb09d59d73020ae607caca22ba22cda58a5
fox@home <jiaqingli@gmail.com>
2014-04-23 07:01:23 -0700

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -86,8 +86,26 @@ class NewVisitorTest(LiveServerTestCase):
         #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         #self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
+    def test_layout_and_styling(self):        
         
-              
-        self.fail('Finish the test')
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
         
-              
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta = 5
+        )  
+        
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta = 5
+        )          
+                
+        #self.fail('Finish the test')
+        
+             
