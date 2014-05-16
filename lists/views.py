b14b353e4c809854c82aec95f4c8c0c6bd95e45e
fox@home <jiaqingli@gmail.com>
2014-05-15 22:45:22 -0700

    rename all item input ids and names. still broken - n ew

--- lists/views.py
+++ lists/views.py
@@ -8,36 +8,27 @@ from django.core.exceptions import ValidationError
 #home_page = None
 
 def home_page(request):
-    
-    
-    #items = Item.objects.all()
-    
+   
     return render(request,'home.html',{'form':ItemForm()})
-    #else:
-    #    new_item_text = ''
-        #return HttpResponse(request.POST['item_text'])
-        
-    
-    #return render(request, 'home.html',{
-    #            'new_item_text': new_item_text, })
-    
+
 def view_list(request,list_id):
     list_ = List.objects.get(id=list_id)
     error = None
     #items = Item.objects.filter(list=list_)
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'],list=list_)
+            item = Item(text=request.POST['text'],list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
+            return render(request,'home.html',{"error":error})
     return render(request, 'list.html',{'list': list_, 'error':error})
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'],list=list_)
+    item = Item(text=request.POST['text'],list=list_)
     try:
         item.full_clean()
         item.save()
