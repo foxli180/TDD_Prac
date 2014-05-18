084839702fbdca1388e99d9ca88578b8de795c6c
fox@home <jiaqingli@gmail.com>
2014-05-18 07:57:51 -0700

    FT and UT workable,End-of-charpter-12-p227

--- lists/forms.py
+++ lists/forms.py
@@ -19,12 +19,15 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text':{'required': EMPTY_LIST_ERROR}
                           }
-        
+
+      
 
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
-    
+
+  
+            
 class ExistingListItemForm(ItemForm):
     
     def __init__(self,for_list,*args,**kwargs):
@@ -37,4 +40,5 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text':[DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
-            
+    def save(self):
+        return forms.models.ModelForm.save(self)   
