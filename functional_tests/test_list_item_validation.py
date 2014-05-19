73193472be3615b4046ca7bc41eee36e660bb412
fox@home <jiaqingli@gmail.com>
2014-05-19 23:06:35 +0800

    using jquery and fixtures div FT pass

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,12 +8,15 @@ from unittest import skip
 
 
 class ItemValidationTest(FunctionalTest):
+    
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error') 
                
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys('\n')
         
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text,"You can't have an empty list item")
         
         self.get_item_input_box().send_keys('Buy Milk\n')
@@ -22,7 +25,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('\n')
         
         self.check_for_row_in_list_table('1: Buy Milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text,"You can't have an empty list item")       
         
         self.get_item_input_box().send_keys('Make Tea\n')
@@ -37,10 +40,20 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Buy wellies\n')
         
         self.check_for_row_in_list_table("1: Buy wellies")
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text,"You've already got this in your list")
         
+    def test_error_message_are_cleared_on_input(self): 
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
         
+        self.get_item_input_box().send_keys('a')
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
+        
+  
         
         
         
